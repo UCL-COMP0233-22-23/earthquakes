41280c372721b5ab58313848bc5d9f6257e4116a
Angus Mingare <ucapafm@ucl.ac.uk>
2022-10-22 12:21:00 +0100

    Finding max earthquake in dataset

--- earthquakes.py
+++ earthquakes.py
@@ -27,31 +27,39 @@ def get_data():
     # To understand the structure of this text, you may want to save it
     # to a file and open it in VS Code or a browser.
     # See the README file for more information.
-    ...
+    import json
+    obj = json.loads(text)
+    with open("earthquakes.json", "w") as file:
+        file.write(json.dumps(obj))
 
     # We need to interpret the text to get values that we can work with.
     # What format is the text in? How can we load the values?
-    return ...
+    return obj["features"] # Returns a list of earthquakes
 
 def count_earthquakes(data):
     """Get the total number of earthquakes in the response."""
-    return ...
+    return len(data)
 
 
 def get_magnitude(earthquake):
     """Retrive the magnitude of an earthquake item."""
-    return ...
+    return earthquake["properties"]["mag"]
 
 
 def get_location(earthquake):
     """Retrieve the latitude and longitude of an earthquake item."""
     # There are three coordinates, but we don't care about the third (altitude)
-    return ...
+    return (earthquake["geometry"]["coordinates"][0], earthquake["geometry"]["coordinates"][1])
 
 
 def get_maximum(data):
     """Get the magnitude and location of the strongest earthquake in the data."""
-    ...
+    mags = [get_magnitude(earthquake) for earthquake in data]
+    locs = [get_location(earthquake) for earthquake in data]
+    max_mag = max(mags)
+    max_idx = mags.index(max_mag)
+    max_loc = locs[max_idx]
+    return max_mag, max_loc
 
 
 # With all the above functions defined, we can now call them and get the result
