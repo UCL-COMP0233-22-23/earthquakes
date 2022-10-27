0c6e7b77b3b9692c1eb70ff84922d1fab34379cd
Nikita Pond <nikita.pond@icloud.com>
2022-10-27 10:40:29 +0100

    Added code to get magnitude, and to produce dict of magnitudes per year

--- plot_earthquakes.py
+++ plot_earthquakes.py
@@ -1,11 +1,42 @@
 from datetime import date
-
+import requests
+import json
 import matplotlib.pyplot as plt
 
 
 def get_data():
     """Retrieve the data we will be working with."""
-    ...
+    # With requests, we can ask the web service for the data.
+    # Can you understand the parameters we are passing here?
+    response = requests.get(
+        "http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
+        params={
+            'starttime': "2000-01-01",
+            "maxlatitude": "58.723",
+            "minlatitude": "50.008",
+            "maxlongitude": "1.67",
+            "minlongitude": "-9.756",
+            "minmagnitude": "1",
+            "endtime": "2018-10-11",
+            "orderby": "time-asc"}
+    )
+
+    # The response we get back is an object with several fields.
+    # The actual contents we care about are in its text field:
+    text = response.text
+    # To understand the structure of this text, you may want to save it
+    # to a file and open it in VS Code or a browser.
+    # See the README file for more information.
+    with open("text_data.json", 'w') as out:
+        out.write(text)
+
+
+    
+    # We need to interpret the text to get values that we can work with.
+    # What format is the text in? How can we load the values?
+    print(type(json.loads(text)))
+    #print(json.loads(text))
+    return json.loads(text)
 
 
 def get_year(earthquake):
@@ -22,7 +53,7 @@ def get_year(earthquake):
 
 def get_magnitude(earthquake):
     """Retrive the magnitude of an earthquake item."""
-    ...
+    return earthquake['properties']['mag']
 
 
 # This is function you may want to create to break down the computations,
@@ -32,21 +63,35 @@ def get_magnitudes_per_year(earthquakes):
     
     Returns a dictionary with years as keys, and lists of magnitudes as values.
     """
-    ...
+    
+    magnitudes = [get_magnitude(quake) for quake in earthquakes]
+    years = [get_year(quake) for quake in earthquakes]
+    
+    result = {}
+    
+    for y, m in zip(years, magnitudes):
+        if y in result:
+            result[y].append(m)
+        else:
+            result[y] = [m]
+            
+    return result
+        
+    
 
 
 def plot_average_magnitude_per_year(earthquakes):
-    ...
+    pass
 
 
 def plot_number_per_year(earthquakes):
-    ...
+    pass
 
 
 
 # Get the data we will work with
 quakes = get_data()['features']
-
+get_magnitudes_per_year(quakes)
 # Plot the results - this is not perfect since the x axis is shown as real
 # numbers rather than integers, which is what we would prefer!
 plot_number_per_year(quakes)
