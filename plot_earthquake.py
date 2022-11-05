facf5f00333e1178a2133d8e2ec7fcf32dca835d
Weixuan Xing <rmapxin@ucl.ac.uk>
2022-11-05 12:15:59 +0000

    get year and magnitude

--- plot_earthquake.py
+++ plot_earthquake.py
@@ -1,11 +1,38 @@
 from datetime import date
-
+import requests
+import json
 import matplotlib.pyplot as plt
 
 
 def get_data():
     """Retrieve the data we will be working with."""
-    ...
+    response = requests.get(
+        "http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
+        params={
+            'starttime': "2000-01-01",
+            "maxlatitude": "58.723",
+            "minlatitude": "50.008",
+            "maxlongitude": "1.67",
+            "minlongitude": "-9.756",
+            "minmagnitude": "1",
+            "endtime": "2018-10-11",
+            "orderby": "time-asc"}
+    )
+
+    # The response we get back is an object with several fields.
+    # The actual contents we care about are in its text field:
+    text = response.text
+    #loads() takes in a string and returns a json object. json. dumps() takes in a json object and returns a string.
+    #save text as json file
+    # To understand the structure of this text, you may want to save it
+    earthquake_data = json.loads(text)
+    with open('earthquake_data.json', 'w',encoding='utf-8') as f:
+        f.write(text)
+    # to a file and open it in VS Code or a browser.
+    # See the README file for more information.
+    # We need to interpret the text to get values that we can work with.
+    # What format is the text in? How can we load the values?
+    return earthquake_data
 
 
 def get_year(earthquake):
@@ -16,13 +43,18 @@ def get_year(earthquake):
     # https://earthquake.usgs.gov/data/comcat/index.php#time
     # Fortunately, Python provides a way of interpreting this timestamp:
     # (Question for discussion: Why do we divide by 1000?)
+    #The fromtimestamp() function is used to return the date corresponding to a specified timestamp.
+    #timestamp in milliseconds, but fronttimestamp expect it in second.divided by 1000
     year = date.fromtimestamp(timestamp/1000).year
     return year
 
 
 def get_magnitude(earthquake):
     """Retrive the magnitude of an earthquake item."""
-    ...
+    return earthquake["properties"]["mag"]
+#print(data['features'][3]['properties']['mag'])
+#need to notice, feature is the key of dictionary
+#the earthquake here stand for list
 
 
 # This is function you may want to create to break down the computations,
