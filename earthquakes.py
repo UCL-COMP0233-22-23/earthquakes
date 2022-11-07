1d0f936329d74b9ff2ae3e9e6a79f7daec1ea309
mariiakzmn <mariiiakuzmina@gmail.com>
2022-11-07 21:53:39 +0100

    Update earthquakes.py
    Swapped imported libraries

--- earthquakes.py
+++ earthquakes.py
@@ -2,8 +2,8 @@
 # over the Internet.
 # However, we will use a more powerful and simpler library called requests.
 # This is external library that you may need to install first.
-import requests
 import pandas as pd
+import requests
 
 
 def get_data():
